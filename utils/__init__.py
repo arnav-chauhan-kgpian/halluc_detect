0b587ac540597ff17bf835b88a212713da233cda
Arnav Chauhan <arnavch246@gmail.com>
2026-03-19 01:17:30 +0530

    minor refactoring

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,23 @@
-# Init file for utils package
+import random
+import os
+import numpy as np
+import torch
+
+def set_seed(seed: int):
+    """Set seeds for reproducibility."""
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    # For newer versions of PyTorch
+    if hasattr(torch, 'use_deterministic_algorithms'):
+        try:
+            torch.use_deterministic_algorithms(True)
+        except RuntimeError:
+            # Some operations may not support deterministic mode
+            # We can disable it or just warn
+            pass
